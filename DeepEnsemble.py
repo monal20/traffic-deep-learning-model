95f4d54e2e8bf95f5fec849c3474060c66b5f6b9
Axel Petit <axel.petit@etu.u-bordeaux.fr>
2023-12-01 10:29:53 +0900

    Adding preprocessing

--- DeepEnsemble.py
+++ DeepEnsemble.py
@@ -38,7 +38,7 @@ offset = 0
 all_data = []
 
 # Fetch data in chunks until there is no more data
-for i in range(20):
+for i in range(2):
     data_chunk = fetch_data(limit=chunk_size, offset=offset)
     
     # Break the loop if no more data
@@ -56,6 +56,43 @@ result_df = pd.concat(all_data, ignore_index=True)
 
 #######################                  PREPROCESS         ##############################
 
+
+
+
+weather_mapping = {
+    'CLEAR': 0,
+    'CLOUDY/OVERCAST': 5,
+    'RAIN': 10,
+    'FOG/SMOKE/HAZE': 20,
+    'SLEET/HAIL': 25,
+    'FREEZING RAIN/DRIZZLE': 30,
+    'SNOW': 40,
+    'BLOWING SNOW': 50,
+}
+
+
+lighting_mapping = {
+    'DAYLIGHT': 0,
+    'DUSK': 15,
+    'DAWN': 15,
+    'DARKNESS, LIGHTED ROAD': 25,
+    'DARKNESS': 30
+}
+
+# Apply the mapping and filter rows
+result_df['lighting_condition'] = result_df['lighting_condition'].map(lighting_mapping)
+result_df = result_df[result_df['lighting_condition'].notna()]
+
+
+# Apply the mapping and filter rows
+result_df['weather_condition'] = result_df['weather_condition'].map(weather_mapping)
+result_df = result_df[result_df['weather_condition'].notna()]
+
+
+print(result_df)
+
+
+"""
 weather_condition_counts = result_df['weather_condition'].value_counts()
 lighting_condition_counts = result_df['lighting_condition'].value_counts()
 street_name_counts = result_df['street_name'].value_counts()
@@ -67,4 +104,4 @@ print("\nLighting Condition Counts:")
 print(lighting_condition_counts) 
 
 print("\nStreet name:")
-print(street_name_counts) 
+print(street_name_counts) """
