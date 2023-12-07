0a4bf15c0378d73d0cf4dee9d4f59c7a31d98c51
Kian Horsmeier <97808709+Kian1208@users.noreply.github.com>
2023-12-07 14:54:58 +0900

    Update mlp.py

--- Models/mlp.py
+++ Models/mlp.py
@@ -3,13 +3,13 @@ from sklearn.neural_network import MLPClassifier
 def mlp_model():
     # Create an MLPClassifier
     mlp = MLPClassifier(
-        hidden_layer_sizes=(64, 32),  # Adjust the architecture as needed
+        hidden_layer_sizes=(32, 16),  # Adjust the architecture as needed
         activation='relu',
         solver='adam',
         alpha=0.0001,
         batch_size='auto',
         learning_rate='constant',
-        learning_rate_init=0.001,
+        learning_rate_init=0.0015,
         max_iter=200,
         random_state=42
     )
